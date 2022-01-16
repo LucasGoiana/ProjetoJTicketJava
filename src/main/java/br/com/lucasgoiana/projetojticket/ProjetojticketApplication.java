e226b2b088779b21949e429e678acdc080f5e948
Lucas Goiana Malicia <lucasgoianam@hotmail.com>
2022-01-16 18:22:01 -0300

    Initial Commit

---
+++ src/main/java/br/com/lucasgoiana/projetojticket/ProjetojticketApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucasgoiana.projetojticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetojticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetojticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucasgoiana/projetojticket/ProjetojticketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasgoiana.projetojticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetojticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
